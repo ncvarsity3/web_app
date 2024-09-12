686189cda3a370975a8f80e9710f177cb9594073
ncvarsity3 <ncvarsity3@gmail.com>
2024-09-12 12:39:42 -0400

    adding dependency information

---
+++ webserver.py
@@ -0,0 +1,9 @@
+'''
+This is my first attempt at creating a Django web app.
+I first cloned the "https://github.com/ncvarsity3/web_app" repository from my personal GitHub account.
+Then created a virtual environment to save my dependencies "python3 -m venv .venv".
+I stored my dependencies in a requirements file with "pip freeze > requirements.txt"
+'''
+
+
+import django
